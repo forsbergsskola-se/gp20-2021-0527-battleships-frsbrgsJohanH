bb0df14b708710c3ec8413961370c812a8198e5e
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2021-06-03 18:29:46 +0200

    formatting number output

--- random_numbers/random_numbers.cpp
+++ random_numbers/random_numbers.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     int numberStorage[100] = {0};
-   
+    int count[21] {0};
         
     srand((unsigned) time(0));  //always changing, will give you a different seed = not the same sequence.
     int randomNum;              
@@ -17,9 +17,20 @@ int main(int argc, char* argv[])
     {
         randomNum = (rand() % 20) + 1;
         numberStorage[i] = randomNum;
+        count[numberStorage[i]]++;
 
-      cout << randomNum << endl;
+        
     }
+
+    for(int i = 0; i < 21; i++)
+    {
+        cout << "number "<<i<< +" ------> "<<count[i]<< + " times"<<endl;
+
+        //cout <<count[i]<< + " occurred"<<i<<endl;
+    }
+
+
+  
 
    
     
